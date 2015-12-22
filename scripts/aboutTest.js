ba34b1b1951862abc64de6564280c09a03c19255
Heidi Laursen <heidilaursen@yahoo.com>
2015-12-22 15:13:30 -0800

    adding state filtering

--- scripts/aboutTest.js
+++ scripts/aboutTest.js
@@ -1,19 +1,34 @@
 congress = {};
+states = {};
 
 congress.stateFilterOptions = function () {
-
+  for (var = i; states.data.length; i++) {
+    var currentState = states.data[i].name;
+    var $popState = ('#filterByState').clone();
+    $popState.removeAttr('id').text(currentState);
+    ('#state').append($popState)
+  }
 }
 
 congress.stateFilterSelect = function () {
   //dropdown filtering trigger here
 }
 
-$.getJSON("congress.json", function(congressPerson) {
-  congressPerson = [];
-  $.forEach(congressPerson, function (sortname, stateFilterSelect) {
-    //retrieve sortname state abbrv.
-    if (sortname = 0) {
+$.getJSON("data/congress.json", function(data, message, xhr) {
+  congress.data = data;
+})
+  .done(function() {
+    congress.data.object.forEach(function(congressPerson) {
+      //retrieve sortname state abbrv. from filter
+      if (congressPerson.sortname.search(/HI/) = 0) {
+        console.log(congressPerson.sortname);
+      };
+    });
+  });
 
-    };
-  };
-});
+  $.getJSON("data/states.json", function(data, message, xhr) {
+    states.data = data;
+  })
+    .done(function (callback) {
+      callback
+    }
