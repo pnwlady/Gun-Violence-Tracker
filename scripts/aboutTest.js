ac736e3c06e288c0e70febf508ee7e81999f258e
Heidi Laursen <heidilaursen@yahoo.com>
2015-12-22 13:06:33 -0800

    about Test site and js

---
+++ scripts/aboutTest.js
@@ -0,0 +1,19 @@
+congress = {};
+
+congress.stateFilterOptions = function () {
+
+}
+
+congress.stateFilterSelect = function () {
+  //dropdown filtering trigger here
+}
+
+$.getJSON("congress.json", function(congressPerson) {
+  congressPerson = [];
+  $.forEach(congressPerson, function (sortname, stateFilterSelect) {
+    //retrieve sortname state abbrv.
+    if (sortname = 0) {
+
+    };
+  };
+});
