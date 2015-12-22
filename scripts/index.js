5aa2229b69a87a728dd9529870d050340c07411c
suhk <kennethgenesuh@gmail.com>
2015-12-22 12:24:46 -0800

    initial setup

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,39 @@
-$(function() {
-  var googleMap = new GoogleMap();
+var index = {};
 
-  $('#continue').on('click', function(event){
-    event.preventDefault();
-    $('.container.textOver').hide();
-    $('.overlay').fadeOut(1000);
+index.modal = function() {
+  $('#modal').modal('show');
+};
+
+index.googleMap = function() {
+  $('#imap').empty();
+  if(index.gMap) {
+    index.gMap.render();
+  } else {
+    index.gMap = new GoogleMap();
+  }
+};
+
+index.intlMap = function() {
+  $('#gmap').empty();
+};
+
+index.takeAction = function() {
+  $('#takeActionModal').modal('show');
+};
+
+$(function() {
+  index.gMap = new GoogleMap();
+
+  var data = {
+    modalName: '',
+    modalLabel: '',
+    title: '',
+    body: ''
+  };
+  $.get('templates/modal.html').done(function(template) {
+    var compiledTemplate = Handlebars.compile(template);
+    var html = compiledTemplate(data);
+    $('#modal').append(html);
   });
 
 });
---
+++ scripts/router.js
@@ -0,0 +1,5 @@
+// page('/', index.modal);
+page('/us', index.googleMap);
+page('/intl', index.intlMap);
+// page('/takeaction', index.takeAction);
+page.start();
